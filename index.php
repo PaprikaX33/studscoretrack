e9155328b34b78a6de824605a221bd9a075347e8
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2022-12-25 00:40:43 +0800

    added the language to the index

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
-<!doctype html>
-<html>
+<?php
+require_once "include/session.php";
+require_once "include/language.php";
+$LANG = config_language();
+?>
+<!DOCTYPE html>
+<html lang=<?php echo "\"" . $LANG["tag"] . "\""?>>
     <head>
     </head>
     <body>
+        <h1><?php echo $LANG["title"];?></h1>
         <?php
-        require_once "include/database.php";
-        echo var_dump(load_json("config/dbconfig.json"));
+        require_once "include/json.php";
+        var_dump(load_json("config/dbconfig.json"));
         ?>
     </body>
     <?php
