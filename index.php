57693e7ec3d73719d78952aee708b1dd5a0f98a0
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2022-12-25 06:36:10 +0800

    added initial header

--- index.php
+++ index.php
@@ -4,14 +4,29 @@ require_once "include/language.php";
 $LANG = new Language();
 ?>
 <!DOCTYPE html>
-<html lang=<?php echo "\"" . $LANG["tag"] . "\""?>>
+<html lang="<?php echo $LANG["tag"] ?>">
     <head>
         <title>
-            <?php echo $LANG->tag?>
+            Score Tracker
         </title>
     </head>
     <body>
-        <h1><?php echo $LANG["title"];?></h1>
+        <header>
+            <!-- Top header menu containing logo and Navigation bar -->
+            <div id="header-title">
+                <nav>
+                    <?php echo $LANG["title"];?>
+                </nav>
+            </div>
+            <div id="header-lang">
+                <nav>
+                    <a href="<?php echo parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?>?lang=en">EN</a>
+                </nav>
+                <nav>
+                    <a href="<?php echo parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?>?lang=zh">ZH</a>
+                </nav>
+        </header>
+        <h1></h1>
         <?php
         require_once "include/json.php";
         var_dump(load_json("config/dbconfig.json"));
