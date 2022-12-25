d4f26cc138717eb6b4cf17f930dab1dff70c5665
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2022-12-26 04:39:10 +0800

    created the debug page

--- debug.php
+++ debug.php
@@ -1,30 +1,33 @@
 <?php
-require_once "include/session.php";
-require_once "include/language.php";
-$LANG = new Language();
+$PAGE_TITLE_TAG = "page-debug";
+require_once "fragment/header.php";
+require_once "include/json.php";
+var_dump(load_json("config/dbconfig.json"));
 ?>
-<!DOCTYPE html>
-<html lang="<?php echo $LANG["tag"] ?>">
-    <head>
-        <title>
-            Score Tracker
-        </title>
-    </head>
-    <body>
-        <?php
-        require_once "fragment/header.php";
-        ?>
-        <h1></h1>
-        <?php
-        require_once "include/json.php";
-        var_dump(load_json("config/dbconfig.json"));
-        ?>
-        <h2><?php echo $LANG["new"] ?></h2>
-    </body>
-    <?php
-    /*
-     * Local Variables:
-     * mode: web
-     * End:
-     * End: */?>
-</html>
+<div class="content-block">
+    <form id="genDB" method="POST" action="/debug_back.php">
+        <input name="act" type="hidden" value="gen" />
+    </form>
+    <form id="delDB" method="POST" action="/debug_back.php">
+        <input name="act" type="hidden" value="del" />
+    </form>
+    <table class="key-val-table">
+        <tbody>
+            <tr>
+                <td>
+                    <input form="genDB" type="submit" value="Create DB"/>
+                </td>
+                <td>
+                    <input form="delDB" type="submit" value="Delete DB"/>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<?php
+require_once "fragment/closer.php";
+/*
+ * Local Variables:
+ * mode: web
+ * End:
+ * End: */?>
