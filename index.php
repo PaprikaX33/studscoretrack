7bcbb7a89ba9fb7d3c44f203b9be28fad2fbd467
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2022-12-26 23:47:20 +0800

    added the automatic table generation in index.
    This is designed to be first page, therefore the one that will check
    the existance of the db, or generate a new db if needed

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
 <?php
+require_once "include/db_con.php";
+$sqlcon = db_init();
+$generatorQ = "
+CREATE TABLE IF NOT EXISTS course (
+id INT NOT NULL AUTO_INCREMENT,
+en_name VARCHAR(50),
+zh_name VARCHAR(50),
+semester INT NOT NULL,
+credit INT NOT NULL DEFAULT 3,
+passing INT NOT NULL DEFAULT 60,
+archived BOOL NOT NULL DEFAULT FALSE,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS test (
+id INT NOT NULL AUTO_INCREMENT,
+courseID INT NOT NULL,
+name VARCHAR(50),
+score INT NOT NULL,
+weight INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (courseID) REFERENCES course(id)
+);";
+$sqlcon->multi_query($generatorQ);
+do{}while($sqlcon->next_result());
+$sqlcon->close();
+
 http_response_code(301);
 header('Location: /course_list.php');
 die();
